e0cacbafc5f91b4dead08a0885e4c89b02df73c5
Atharv Pandit <atharvpanditinteresting@gmail.com>
2026-06-11 11:42:41 +0800

    Create WaterTrigger.cs

---
+++ WaterTrigger.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class WaterTrigger : MonoBehaviour
+{
+    private GameRules gameRules;
+
+    void Start()
+    {
+        gameRules =
+            FindFirstObjectByType<GameRules>();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameRules.PlayerTouchedWater();
+        }
+    }
+}
